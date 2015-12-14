2e6be85f7b9525b4aec686784e6a0c7f52837654
prieta1975 <antonio_prieto_jimenez@hotmail.com>
2015-12-14 20:58:29 +0100

    Primera versión funcional

--- main.py
+++ main.py
@@ -2,3 +2,14 @@
 import numpy as np
 import cv2
 
+cap = cv2.VideoCapture(0)
+
+while (True):
+    ret, frame = cap.read()
+
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
